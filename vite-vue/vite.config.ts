3734e70ceac1805f25f8b859b9041ca3fc836b1a
JiuMu <17681086218@163.com>
2022-04-12 23:40:08 +0800

    修改前端项目配置

--- vite-vue/vite.config.ts
+++ vite-vue/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  // 检测process.cwd()路径下的.env.development.local、.env.development、.env.local、.env这四个环境文件。
+  // 输出NODE_ENV和VITE_开头的键值对，
+  // VITE_开头的键值对后面的不会覆盖前面的，NODE_ENV的值后面的会覆盖前面的
+  var env = loadEnv(mode, root);
+  return {
+    server: {
+      port: 3001
+    },
+    plugins: [vue()] 
+  };
 })
