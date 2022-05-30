f46d605a00b5569aadd553c9d3c0660feaff1b75
gxl <17681086218@163.com>
2022-05-30 11:12:02 +0800

    修改后台接口

--- NeteaseCloudMusicApi/app.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const generateConfig = require('./generateConfig')
-async function start() {
-  await generateConfig()
-  require('./server').serveNcmApi({
-    checkVersion: true,
-  })
-}
-start()
