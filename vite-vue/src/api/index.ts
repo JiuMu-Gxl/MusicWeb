7a2693a7f6ad06c7fe8e05fc72501dc63a9322ff
gxl <17681086218@163.com>
2022-06-21 16:26:44 +0800

    添加全局请求Loading

--- vite-vue/src/api/index.ts
+++ vite-vue/src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Toast } from 'vant';
 
 // 创建axios实例 请求
 let service: any = axios.create({
@@ -9,6 +10,11 @@ let service: any = axios.create({
 // 请求拦截器
 service.interceptors.request.use((config: any) => {
     // 在发送请求之前做些什么
+    // 请求等待Loading
+    Toast.loading({
+        message: '加载中...',
+        forbidClick: true
+    });
     return config;
 },(error: any) =>{
     console.log(error, "error-request");
@@ -17,6 +23,7 @@ service.interceptors.request.use((config: any) => {
 // 响应拦截器
 service.interceptors.response.use((response: any) => {
     // 对响应数据做点什么
+    Toast.clear();
     switch (response.status) {
         case 200:
             return response.data;
